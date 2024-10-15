623c9a67bbf7985c75faeebe2ef875829ae39180
Shresht44 <ironbee22@gmail.com>
2024-10-15 21:06:45 +0530

    Update concatenation of string.cpp

--- concatenation of string.cpp
+++ concatenation of string.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main()
 {
     string first_name,last_name;
-    first_name = "Soham";
-    last_name = "Jade";
+    first_name = "L.";
+    last_name = "Shresht";
 
     cout<<first_name+last_name;
 }
 
 /*
-SohamJade
+L.Shresht
 */
